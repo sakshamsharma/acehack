01b3b0bff26a9e208509662cc8e0dfb0a5c71c16
Saksham Sharma <saksham0808@gmail.com>
2016-05-26 20:10:59 +0200

    Remove docker build rules

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
-docker pull sakshamsharma/docker-hakyll
-contId=$(docker run -d sakshamsharma/docker-hakyll https://github.com/sakshamsharma/acehack)
-docker wait ${contId}
-docker cp -r ${contId}:/home/hakyll/clone/_site/* _site/
+stack exec site build
 ./deploy.sh
