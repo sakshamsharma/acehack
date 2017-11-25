871fe719354bbbae2187ba597dd544d02d2fb764
Travis CI User <travis@example.org>
2017-11-25 23:47:02 +0000

    publish: Add new logo
    generated from commit a003319c45f1a4ff7cad145c93936c5031b41652

--- js/archive.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-   $(".postShorten-wrap").map(function() {
-       $(this).click(function() {
-           redirect($(this).attr('id').substring(5));
-       });
-   });
-});
